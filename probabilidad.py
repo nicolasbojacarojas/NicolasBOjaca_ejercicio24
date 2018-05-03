18a90c0dbb960e90081e0a123356b8abfaf5b07c
nicobojacarojas <nicobojacarojas@hotmail.com>
2018-05-03 09:00:05 -0500

    segunda version

--- probabilidad.py
+++ probabilidad.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 #arreglo con los x dados en donde caen las particulas
 x = np.array([1.2, 2.5, 2.8, 5.0])
 #probabilidad de pl asignada arbitrariamente
-pl = 1
+pl = 0.3	
 #medida del vidrio en donde caen las particulas
 x0 = 1
 x1 = 20
@@ -14,13 +14,17 @@ def nnorm(pl, x0, x1, lam):
 def fun(pl, lam, n, x):
 	return pl/(n*lam)*(np.exp(-x/lam))
 #lambdas distintos a evaluar y a graficar
-lam = np.linspace(1, 10, 10)
+lam = np.linspace(0.1, 1000)
+a = 0
+for i in range(len(x)):
+	a += x[i]
+y= []
 #generacion de grafica para distintos lambdas
 for i in range(len(lam)):
 	n = nnorm(pl, x0, x1, lam[i])
-	plt.plot(x, fun(pl, lam[i], n, x), label = "lambda = " + str(lam[i]) )
-	plt.xlabel("posicion en x")
-	plt.ylabel("probabilidad de lambda")
-	plt.legend(loc = 0)
-plt.savefig("grafica")
+	y.append(fun(pl, lam[i], n, a/len(x)))
+plt.plot(lam, y)
+plt.xlabel("lambda")
+plt.ylabel("pobabilidad")
+plt.show()
 
